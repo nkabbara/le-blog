736325ef0570e59c904950d531ff8b828525c197
Nash Kabbara <nash@zipzoomauto.com>
2015-02-21 18:29:46 -0600

    Added basic auth. With public password! Yikes!

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,6 @@
 class ArticlesController < ApplicationController
+  http_basic_authenticate_with name: 'nash', password: 'mypass123', except: [:index, :show]
+
   def new
     @article = Article.new
   end
